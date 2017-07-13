7fbd7e13e367bd504ddfc5df35606c6196544cad
Puskiiin <pushking34@gmail.com>
2017-07-13 19:27:52 +0300

    from170629 added

---
+++ src/homework/from170629/Genome.java
@@ -0,0 +1,22 @@
+package homework.from170629;
+
+
+// the task is to repeat duplicates search algorithm with iterations
+// actually we made a huge mistake at class, so I'll write new one later probably
+
+public class Genome {
+
+    public static final int DATA_SIZE = 30;
+
+    public static void main(String[] args) {
+        byte[] data = DataGen.generate(DATA_SIZE);
+        print(data);
+
+    }
+
+    public static void print(byte[] arr){
+        for (byte b : arr) {
+            System.out.print((char)b);
+        }
+    }
+}
