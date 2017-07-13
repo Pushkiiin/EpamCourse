875c6d10b87887079a4195530fb1e35a3701869e
Puskiiin <pushking34@gmail.com>
2017-07-13 21:44:36 +0300

    from170712 added; Stack and Test classes created

---
+++ src/homework/from170712/MyStack.java
@@ -0,0 +1,4 @@
+package homework.from170712;
+
+public class MyStack {
+}
---
+++ src/homework/from170712/StackTest.java
@@ -0,0 +1,8 @@
+package homework.from170712;
+
+public class StackTest {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+
+    }
+}
