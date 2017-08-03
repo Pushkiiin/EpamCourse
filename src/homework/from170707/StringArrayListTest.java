e8db090f8d4ad541c55531e43a3081c2f4d1fe2c
Puskiiin <pushking34@gmail.com>
2017-08-03 19:50:37 +0300

    from170707 and from170710 tests updated

--- src/homework/from170707/StringArrayListTest.java
+++ src/homework/from170707/StringArrayListTest.java
@@ -1,26 +1,25 @@
 package homework.from170707;
 
+import mytestframework.Asserts;
+
 public class StringArrayListTest {
     public static void main(String[] args) {
         StringArrayList list = new StringArrayList();
         list.add("qwe");
         list.add("asd");
         list.add("zxc");
+        System.out.println(Asserts.assertEquals(list.toString(), "[qwe, asd, zxc]"));
         list.set(1, "dsa");
         list.set(4, "dsa");
-        System.out.println(list.toString());
-        System.out.println(list.size());
-        System.out.println(list.isEmpty());
-        System.out.println(list.get(2));
+        System.out.println(Asserts.assertEquals(list.toString(), "[qwe, dsa, zxc]"));
+        System.out.println(Asserts.assertEquals(list.get(2), "zxc"));
         list.remove(0);
-        list.remove(5);
         list.remove("zxc");
-        System.out.println(list.toString());
+        System.out.println(Asserts.assertEquals(list.toString(), "[dsa]"));
 
         list = new StringArrayList();
-        System.out.println(list.isEmpty());
-        list.set(1, "asd");
-        System.out.println(list.size());
-        System.out.println(list.toString());
+        System.out.println(Asserts.assertEquals(list.isEmpty(), true));
+        list.set(0, "asd");
+        System.out.println(Asserts.assertEquals(list.toString(), "[]"));
     }
 }
--- src/homework/from170710/StringLinkedListTest.java
+++ src/homework/from170710/StringLinkedListTest.java
@@ -1,21 +1,21 @@
 package homework.from170710;
 
+import mytestframework.Asserts;
+
 public class StringLinkedListTest {
     public static void main(String[] args) {
         StringLinkedList list = new StringLinkedList();
-        System.out.println(list.toString());
+        System.out.println(Asserts.assertEquals(list.toString(), "[]"));
         list.add("0");
         list.add("1");
         list.add("2");
         list.add("3");
         list.add("4");
         list.add("5");
-        System.out.println(list.toString());
-        System.out.println(list.remove(6));
-        System.out.println(list);
-        System.out.println(list.remove(3));
-        System.out.println(list);
-        System.out.println(list.remove(0));
-        System.out.println(list);
+        System.out.println(Asserts.assertEquals(list.toString(), "[0, 1, 2, 3, 4, 5]"));
+        System.out.println(Asserts.assertEquals(list.remove(3), "3"));
+        System.out.println(Asserts.assertEquals(list.toString(), "[0, 1, 2, 4, 5]"));
+        System.out.println(Asserts.assertEquals(list.remove(0), "0"));
+        System.out.println(Asserts.assertEquals(list.toString(), "[1, 2, 4, 5]"));
     }
 }
