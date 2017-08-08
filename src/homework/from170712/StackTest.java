b141da9a59e2f84fad642cdaa9605472a99cf407
Puskiiin <pushking34@gmail.com>
2017-08-08 21:28:24 +0300

    from170712 TDD add(String) requested

--- src/homework/from170712/StackTest.java
+++ src/homework/from170712/StackTest.java
@@ -2,6 +2,9 @@ package homework.from170712;
 
 public class StackTest {
     public static void main(String[] args) {
-        System.out.println("Initial");
+        MyStringStack stack = new MyStringStack();
+        stack.add("qwe");
+        stack.add("asd");
+        stack.add("zxc");
     }
 }
