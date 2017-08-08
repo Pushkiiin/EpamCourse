94e026d214bd25faf5512685d82afff7bd642008
Puskiiin <pushking34@gmail.com>
2017-08-08 21:42:02 +0300

    from170712 TDD 2nd test passed

--- src/homework/from170712/MyStringStack.java
+++ src/homework/from170712/MyStringStack.java
@@ -1,12 +1,15 @@
 package homework.from170712;
 
 public class MyStringStack {
+    private static final int CAPACITY = 4;
+    private final String[] elements = new String[CAPACITY];
+    private int size = 0;
 
     public void add(String str) {
-
+        elements[size++] = str;
     }
 
     public String peek() {
-        return "zxc";
+        return elements[size - 1];
     }
 }
