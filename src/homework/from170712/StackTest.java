c0d856dd2c06086d24d4d7cb3161b35aef7277e7
Puskiiin <pushking34@gmail.com>
2017-08-08 21:33:15 +0300

    from170712 TDD peek() requested

--- src/homework/from170712/StackTest.java
+++ src/homework/from170712/StackTest.java
@@ -1,10 +1,13 @@
 package homework.from170712;
 
+import mytestframework.Asserts;
+
 public class StackTest {
     public static void main(String[] args) {
         MyStringStack stack = new MyStringStack();
         stack.add("qwe");
         stack.add("asd");
         stack.add("zxc");
+        Asserts.assertEquals(stack.peek(), "zxc");
     }
 }
