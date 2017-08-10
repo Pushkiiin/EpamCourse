2f6a3d292438913233bf9d8a8f22e600d2c8ac69
Puskiiin <pushking34@gmail.com>
2017-08-10 18:48:30 +0300

    hackerrank day5 done

---
+++ src/hackerrank30days/day5/Solution.java
@@ -0,0 +1,15 @@
+package hackerrank30days.day5;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + n*i);
+        }
+    }
+}
