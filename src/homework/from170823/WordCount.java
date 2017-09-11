fa237552f375d2fe7acd465475fa6d735d6b1ce6
Puskiiin <pushking34@gmail.com>
2017-09-11 15:02:25 +0300

    from170823 refactored

--- src/homework/from170823/WordCount.java
+++ src/homework/from170823/WordCount.java
@@ -5,23 +5,11 @@ import java.util.*;
 //string list into map(string : int); loop vs lambda
 public class WordCount {
 
-    //helper class
-    static class StrListGenerator{
-        public static List<String> generate(int size){
-            List<String> strList = new ArrayList<>();
-            Random rnd = new Random();
-            String[] values = {"one", "two", "three", "four", "five", "many"};
-
-            for (int i = 0; i < size; i++) {
-                strList.add(values[rnd.nextInt(6)]);
-            }
-            return strList;
-        }
-    }
+    private static final int strLength = 20;
 
     //actual code
     public static void main(String[] args) {
-        List<String> strList = StrListGenerator.generate(20);
+        List<String> strList = StrListGenerator.generate(strLength);
         System.out.println(strList);
 
         //loop solution
@@ -35,6 +23,22 @@ public class WordCount {
         Map<String, Integer> lambdaMap = new HashMap<>();
         strList.forEach(s -> lambdaMap.put(s, lambdaMap.containsKey(s) ? lambdaMap.get(s) + 1 : 1));
         System.out.println(lambdaMap);
+    }
 
+
+    //helper class
+    static class StrListGenerator{
+
+
+        public static List<String> generate(int size){
+            List<String> strList = new ArrayList<>();
+            Random rnd = new Random();
+            String[] values = {"one", "two", "three", "four", "five", "many"};
+
+            for (int i = 0; i < size; i++) {
+                strList.add(values[rnd.nextInt(6)]);
+            }
+            return strList;
+        }
     }
 }
